92719a7af6bec851ca25b63bab1dd39f006c95d5
iMan <iman.nickaein74@gmail.com>
2018-08-09 23:32:31 +0430

    room list added

--- app/src/main/java/iman/reserve/hotel/model/Room.java
+++ app/src/main/java/iman/reserve/hotel/model/Room.java
@@ -3,40 +3,68 @@ package iman.reserve.hotel.model;
 import com.google.gson.annotations.SerializedName;
 
 public class Room {
-    @SerializedName("name")
-    private String name;
-    @SerializedName("email")
-    private String email;
-    @SerializedName("phone")
-    private String phone;
+    @SerializedName("h_id")
+    private String h_id;
 
-    public Room(String name, String email, String phone) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
+    @SerializedName("type")
+    private String type;
+    @SerializedName("food")
+    private String food;
+    @SerializedName("price")
+    private String price;
+    @SerializedName("avroom")
+    private String avroom;
+    @SerializedName("size")
+    private String size;
+    @SerializedName("totroom")
+    private String totroom;
+
+    public Room(String type, String food, String price, String avroom, String size) {
+        this.type = type;
+        this.food = food;
+        this.price = price;
+        this.avroom = avroom;
+        this.size = size;
     }
 
-    public String getName() {
-        return name;
+    public String getType() {
+        return type;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getEmail() {
-        return email;
+    public String getFood() {
+        return food;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setFood(String food) {
+        this.food = food;
     }
 
-    public String getPhone() {
-        return phone;
+    public String getPrice() {
+        return price;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public void setPrice(String price) {
+        this.price = price;
     }
+
+    public String getAvroom() {
+        return avroom;
+    }
+
+    public void setAvroom(String avroom) {
+        this.avroom = avroom;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
 }
